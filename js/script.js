b9f55b3e4db08cbbfa3fdd677a4b4280742802dd
Jordan <jpkronquist@gmail.com>
2020-06-02 14:05:27 -0700

    add toggle function to click.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,6 @@
-jQuery("h1").click(function() {
-  alert("This is a header.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("this is an image.");
-});
-
-jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-  });
-  
+$(document).ready(function() {
+   $("p").click(function() {
+     $(".walrus-showing").toggle();
+     $(".walrus-hidden").toggle();
+   });
+});  
