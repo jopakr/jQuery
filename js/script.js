0c401e7da02947b493d883fcb42bea52ee311b1b
Jordan <jpkronquist@gmail.com>
2020-06-02 13:28:58 -0700

    move the <script>s up into the header.

--- js/script.js
+++ js/script.js
@@ -9,3 +9,9 @@ jQuery("p").click(function() {
 jQuery("img").click(function() {
   alert("this is an image.");
 });
+
+jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+  
